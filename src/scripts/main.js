588c7ed30e562cf4b69cd60aa5015f661b8bd160
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-06 14:29:35 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const thead = document.querySelector('thead');
+  const tfoot = document.querySelector('tfoot');
+  const tbody = document.querySelector('tbody');
+
+  function addColumn(section) {
+    const tr = section.querySelector('tr');
+    const th = document.createElement('th');
+    const clone = tr.children[1].textContent;
+
+    th.textContent = clone;
+    tr.insertBefore(th, tr.children[4]);
+  }
+
+  function cloneColumn(section) {
+    const trAll = Array.from(section.querySelectorAll('tr'));
+
+    trAll.forEach((tr) => {
+      const clone = tr.children[1];
+      const td = clone.cloneNode(true);
+
+      tr.insertBefore(td, tr.children[tr.children.length - 1]);
+    });
+  }
+
+  addColumn(thead);
+  addColumn(tfoot);
+  cloneColumn(tbody);
+});

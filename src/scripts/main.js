bdeb0792adacb813fedd74c62f937247dcec38bb
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-06 15:03:09 +0100

    comments solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,27 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
   const tfoot = document.querySelector('tfoot');
   const tbody = document.querySelector('tbody');
 
-  function addColumn(section) {
-    const tr = section.querySelector('tr');
-    const th = document.createElement('th');
-    const clone = tr.children[1].textContent;
-
-    th.textContent = clone;
-    tr.insertBefore(th, tr.children[4]);
-  }
-
   function cloneColumn(section) {
     const trAll = Array.from(section.querySelectorAll('tr'));
 
     trAll.forEach((tr) => {
-      const clone = tr.children[1];
-      const td = clone.cloneNode(true);
+      if (tr.children.length > 1) {
+        const clone = tr.children[1];
+        const cell = clone.cloneNode(true);
 
-      tr.insertBefore(td, tr.children[tr.children.length - 1]);
+        tr.insertBefore(cell, tr.children[tr.children.length - 1]);
+      }
     });
   }
 
-  addColumn(thead);
-  addColumn(tfoot);
+  cloneColumn(thead);
+  cloneColumn(tfoot);
   cloneColumn(tbody);
 });
